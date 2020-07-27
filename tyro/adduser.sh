bb2ddbdb5c1c8d9717d10069308c553a9c50b068
Brent Smith <bsmith515@gmail.com>
2020-07-27 16:39:32 -0400

    updating cert files location

--- tyro/adduser.sh
+++ tyro/adduser.sh
@@ -10,5 +10,5 @@ else
   exit 1
 fi
 
-kubectl config set-credentials drogo --client-certificate=drogo.crt --client-key=drogo.key
+kubectl config set-credentials drogo --client-certificate=/root/drogo.crt --client-key=/root/drogo.key
 kubectl config set-context developer --cluster=kubernetes --namespace=development --user=drogo
